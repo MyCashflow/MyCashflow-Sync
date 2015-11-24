a49fd7b6e2464035c3739d6e191c1ee83533a1e2
Jukka Koskinen <jukka@koskinen.io>
2015-11-24 15:16:20 +0200

    Bump version

--- bin/cli.js
+++ bin/cli.js
@@ -5,7 +5,7 @@ const Program = require('commander')
 const Prompt = require('prompt')
 const Syncer = require('../lib/syncer')
 
-Program.version('0.1.1')
+Program.version('0.1.2')
 Prompt.colors = false
 Prompt.message = '> '.green
 Prompt.delimiter = ''
